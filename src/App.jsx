30b5b894eb19b0066c69fc55a9c80528ad5aebb6
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-12-05 01:28:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import CartWidget from './components/CartWidget'
+
+
+
+const App = () => {
+  return (<>
+    <Navbar />
+    <CartWidget />
+  </>
+  )
+
+
+}
+
+export default App
+
